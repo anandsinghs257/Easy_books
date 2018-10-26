e3ebb63add3cfef45edc98dd0c540b67d76d2b67
anandsinghs257 <anandsinghs257@gmail.com>
2018-10-27 03:00:08 +0530

    add_admin_devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :books
   devise_for :users
+
+
+  namespace :admin do 
+  	resources :books
+  end
+
+  root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
